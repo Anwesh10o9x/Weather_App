b7128243e045ac4c61b772e2defed81f06c476f1
Anwesh <appleapple@apples-Air.domain.name>
2020-06-01 20:04:08 +0530

    Final

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,5 @@
 <template>
-  <div id="app">
+  <div id="app" :class="typeof weather.main != 'undefined' && weather.main.temp > 16 ? 'warm' : ''">
     <main>
       <div class="search-box">
         <input 
@@ -13,10 +13,10 @@
       <div class="weather-wrap" v-if="typeof weather.main != 'undefined'">
         <div class="location-box">
           <div class="location">{{ weather.name }}, {{ weather.sys.country }}</div>
-          <div class="date">Monday 20 January 2020</div>
+          <div class="date">{{ dateBuilder() }}</div>
           <div class="weather-box">
-            <div class="temperature">9°c</div>
-            <div class="weather">Rain</div>
+            <div class="temperature">{{ Math.round(weather.main.temp) }}</div>
+            <div class="weather">{{ weather.weather[0].main }}</div>
           </div>
         </div>
       </div>
@@ -42,7 +42,7 @@ export default {
   methods: {
     fetchWeather (e) {
       if(e.key  == "Enter"){
-        fetch(`${this.url_base}/weather?q=${this.query}&APPID=${this.api_key}`)
+        fetch(`${this.url_base}/weather?q=${this.query}&units=metric&APPID=${this.api_key}`)
         .then(res => {
           return res.json();
         }).then(this.setResults);
@@ -51,6 +51,20 @@ export default {
     },
     setResults(results){
       this.weather = results;
+
+    },
+    dateBuilder() {
+      let d = new Date();
+      let months = ["January","February","March","April","May","June","July","August",
+                    "September","October","November","December"];
+      let days = ["Monday","Tuesday","Wednesday","Thursday","Friday","Saturday"];
+
+      let day = days[d.getDay()];
+      let date = d.getDate();
+      let month = months[d.getMonth()];
+      let year = d.getFullYear();
+
+      return `${day} ${date} ${month} ${year}`;
 
     }
 
@@ -72,6 +86,9 @@ body{
   background-size: cover;
   background-position: bottom;
   transition: 0.4s;
+}
+#app.warm{
+  background-image: url(./assets/warm-bg1.jpg);
 }
 main{
   min-height: 100vh;
@@ -137,8 +154,6 @@ main{
   font-weight: 700;
   font-style: italic;
   text-shadow: 3px 6px rgba(0,0,0,0.25);
-
-
 }
   
 
